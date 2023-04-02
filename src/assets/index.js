bb15c2b3aadb58853621d08e74abb147ab72a8c7
karan shah <mailtokks290199@gmail.com>
2023-04-02 23:10:48 +0530

    Added minor changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,5 @@
 import logo from "./logowhite.svg";
 import backend from "./backend.png";
-import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
 import menu from "./menu.svg";
@@ -26,7 +25,6 @@ import chubb from "./company/chubb.jpg";
 export {
   logo,
   backend,
-  creator,
   mobile,
   web,
   menu,
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,7 +4,6 @@ import { technologies } from "../constants";
 import { motion } from "framer-motion";
 import { SectionWrapper } from "../hoc";
 import { textVariant } from "../utils/motion";
-import { BallCanvas } from "./canvas";
 
 const Tech = () => {
   return (
@@ -17,8 +16,8 @@ const Tech = () => {
       </motion.div>
       <div className="flex flex-row flex-wrap justify-center gap-10">
         {technologies.map((technology) => (
-          <div className="w-28 h-28" key={technology.name}>
-            <BallCanvas id = {technology.name} icon={technology.icon}/>
+          <div className="w-28 h-28 flex flex-col rounded-2xl justify-center items-center" key={technology.name}>
+            <img src={technology.icon} alt={technology.name} className="w-20 h-20 object-contain"/>
             <div className="flex justify-center items-center">
               <p
                 className="text-secondary text-[16px] font-semibold"
